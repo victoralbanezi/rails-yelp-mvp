331a7758168fee0d4c36f1208bad5f19e4907657
victor albanezi <victor.albanezi@hotmail.com>
2020-04-23 00:35:27 -0300

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:create, :index, :new, :show] do
+    resources :reviews, only: [:new, :index]
+  end
 end
